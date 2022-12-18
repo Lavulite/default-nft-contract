ef1d276a858e951cf0a380273d7d268c5edbb9bb
lavulite <lavulite.eng@gmail.com>
2022-12-18 07:16:14 +0000

    一通りの機能実装

--- contracts/tokens/FungibleSBT/BasicFungibleSBT.sol
+++ contracts/tokens/FungibleSBT/BasicFungibleSBT.sol
@@ -1,22 +1,17 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.0 <0.9.0;
 
-import "../../libs/donate/DonateWithdraw.sol";
 import "../../libs/TokenSupplier/TokenUriSupplier.sol";
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
-import "erc721-multi-sales/contracts/multi-wallet/merkletree/ERC721MultiSaleByMerkleMultiWallet.sol";
 
-// TODO: セールの改善・寄付受付追加
 contract BasicFungibleSBT is
     ERC1155,
     Ownable,
     AccessControl,
-    DonateWithdraw,
-    TokenUriSupplier,
-    ERC721MultiSaleByMerkleMultiWallet
+    TokenUriSupplier
 {
     using Strings for uint256;
 
@@ -24,12 +19,8 @@ contract BasicFungibleSBT is
     string private _baseUri;
     string private _baseExtension = ".json";
 
-    constructor(string memory baseUri, address[] memory admins) ERC1155("") {
-        _baseUri = baseUri;
-
-        for (uint256 i = 0; i < admins.length; i++) {
-            _grantRole(ADMIN, admins[i]);
-        }
+    constructor() ERC1155("") {
+        _grantRole(ADMIN, msg.sender);
     }
 
     modifier onlyTokenOwner(uint256 id) {
@@ -37,9 +28,14 @@ contract BasicFungibleSBT is
         _;
     }
 
-    function mint(address[] calldata to, uint256 id) external onlyRole(ADMIN) {
+    function airdrop(
+        address[] calldata to,
+        uint256[] calldata amount,
+        uint256 id
+    ) external onlyRole(ADMIN) {
+        require(to.length == amount.length);
         for (uint256 i = 0; i < to.length; i++) {
-            _mint(to[i], id, 1, "");
+            _mint(to[i], id, amount[i], "");
         }
     }
 
@@ -59,8 +55,11 @@ contract BasicFungibleSBT is
             ERC1155.supportsInterface(interfaceId);
     }
 
+    // ==================================================================
+    // For SBT
+    // ==================================================================
     function setApprovalForAll(address, bool) public virtual override {
-        require(false, "This token is SBT, so this can not approve.");
+        revert("This token is SBT, so this can not approve.");
     }
 
     function _beforeTokenTransfer(
@@ -77,92 +76,6 @@ contract BasicFungibleSBT is
                 "This token is SBT, so this can not transfer."
             );
         }
-    }
-
-    // ==================================================================
-    // override ERC721MultiSaleByMerkle
-    // ==================================================================
-    function airdrop(
-        address[] calldata to,
-        uint256[] calldata amount,
-        uint256 id
-    ) external onlyRole(ADMIN) {
-        require(to.length == amount.length);
-        for (uint256 i = 0; i < to.length; i++) {
-            _mint(to[i], id, amount[i], "");
-        }
-    }
-
-    // == For sale ==
-    function claim(
-        uint256, /*userId*/
-        uint248, /*amount*/
-        uint248, /*allowedAmount*/
-        bytes32[] calldata /*merkleProof*/
-    ) external payable {
-        revert("Not implemented");
-    }
-
-    function claim(
-        uint256 userId,
-        uint248 amount,
-        uint248 allowedAmount,
-        uint256 id,
-        bytes32[] calldata merkleProof
-    )
-        external
-        payable
-        whenNotPaused
-        isNotOverAllowedAmount(userId, amount, allowedAmount)
-        whenClaimSale
-        enoughEth(amount)
-        hasRight(userId, amount, allowedAmount, merkleProof)
-    {
-        _record(userId, amount);
-        _mint(msg.sender, id, amount, "");
-    }
-
-    function exchange(
-        uint256, /*userId*/
-        uint256[] calldata, /*burnTokenIds*/
-        uint248, /*allowedAmount*/
-        bytes32[] calldata /*merkleProof*/
-    ) external payable {
-        revert("Not implemented");
-    }
-
-    function setCurrentSale(Sale calldata sale, bytes32 merkleRoot)
-        external
-        onlyRole(ADMIN)
-    {
-        _setCurrentSale(sale);
-        _merkleRoot = merkleRoot;
-    }
-
-    // ==================================================================
-    // override BasicSale
-    // ==================================================================
-    function pause() external onlyRole(ADMIN) {
-        _pause();
-    }
-
-    function unpause() external onlyRole(ADMIN) {
-        _unpause();
-    }
-
-    function setWithdrawAddress(address payable value)
-        external
-        onlyRole(ADMIN)
-    {
-        withdrawAddress = value;
-    }
-
-    function setMaxSupply(uint256 value) external onlyRole(ADMIN) {
-        maxSupply = value;
-    }
-
-    function _totalSupply() internal pure override returns (uint256) {
-        revert("No use.");
     }
 
     // ==================================================================
@@ -200,30 +113,5 @@ contract BasicFungibleSBT is
         onlyRole(ADMIN)
     {
         externalSupplier = ITokenUriSupplier(_value);
-    }
-
-    // ==================================================================
-    // override DonateWithdraw
-    // ==================================================================
-    function withdraw() external payable override onlyRole(ADMIN) {
-        _withdraw();
-    }
-
-    function _withdraw() internal override(BasicSale, DonateWithdraw) {
-        DonateWithdraw._withdraw();
-    }
-
-    function _withdrawAddress()
-        internal
-        view
-        override
-        returns (address payable)
-    {
-        return withdrawAddress;
-    }
-
-    function setDonationRate(uint256 value) public onlyRole(ADMIN) {
-        require(value <= MAX_DONATION_RATE, "donation rate is over 100%");
-        donationRate = value;
     }
 }
---
+++ contracts/tokens/FungibleSBT/SampleBasicFungibleSBT.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.0 <0.9.0;
+
+import "./BasicFungibleSBT.sol";
+
+contract SampleBasicFungibleSBT is BasicFungibleSBT {}
